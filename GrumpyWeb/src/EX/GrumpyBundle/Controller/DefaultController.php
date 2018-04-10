57f5d4d5eb6f1719bbc5227f34c4cdb5303c9dc5
Paul <abonnementspaul@gmail.com>
2018-04-10 19:52:33 +0200

    Hello world

--- GrumpyWeb/src/EX/GrumpyBundle/Controller/DefaultController.php
+++ GrumpyWeb/src/EX/GrumpyBundle/Controller/DefaultController.php
@@ -6,9 +6,49 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
-    {
-        # return $this->render('EXGrumpyBundle:Default:index.html.twig');
-        return $this->render("@EXGrumpy/Default/index.html.twig");
+ public function indexAction()
+  {
+
+    $listAdverts = array(
+      array(
+        'title'   => 'Grosse boite',
+        'id'      => 1,
+        'author'  => 'Alexandre',
+        'content' => 'Nous recherchons un développeur Symfony2 débutant sur Lyon. Blabla…',
+        'date'    => new \Datetime()),
+      array(
+        'title'   => 'Manger des gauffres',
+        'id'      => 2,
+        'author'  => 'Hugo',
+        'content' => 'Nous recherchons un webmaster capable de maintenir notre site internet. Blabla…',
+        'date'    => new \Datetime()),
+      array(
+        'title'   => 'L\'art de la pose clope inopinée',
+        'id'      => 3,
+        'author'  => 'Nassim',
+        'content' => 'Nous proposons un poste pour webdesigner. Blabla…',
+        'date'    => new \Datetime())
+    );
+    // Mais pour l'instant, on ne fait qu'appeler le template
+    return $this->render("@EXGrumpy/Advert/index.html.twig", array(
+      'listAdverts' => $listAdverts
+    ));
+  }
+
+  public function menuAction($limit)
+  {
+    // On fixe en dur une liste ici, bien entendu par la suite
+    // on la récupérera depuis la BDD !
+    $listAdverts = array(
+      array('id' => 2, 'title' => 'Recherche développeur Symfony'),
+      array('id' => 5, 'title' => 'Mission de webmaster'),
+      array('id' => 9, 'title' => 'Offre de stage webdesigner')
+    );
+
+    return $this->render("@EXGrumpy/Advert/menu.html.twig", array(
+      // Tout l'intérêt est ici : le contrôleur passe
+      // les variables nécessaires au template !
+      'listAdverts' => $listAdverts
+    ));
     }
 }
