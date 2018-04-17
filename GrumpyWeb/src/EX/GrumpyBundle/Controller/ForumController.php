597d3b5ebc09a2a278e02afd5631440b6929aee6
GhislainGloria <ghislain.gloria@viacesi.fr>
2018-04-17 19:09:55 +0200

    controller added

--- GrumpyWeb/src/EX/GrumpyBundle/Controller/ForumController.php
+++ GrumpyWeb/src/EX/GrumpyBundle/Controller/ForumController.php
@@ -66,59 +66,9 @@ class ForumController extends Controller
 				);
 		}
 
-		public function add_productAction(Request $request)
-		{
-
-			$produit = new Produit();
-			$form = $this->createForm(ProduitType::class, $produit);
 
 
-			$form->handleRequest($request);
 
-			if ($form->isSubmitted() && $form->isValid()) {
-
-						$entityManager = $this->getDoctrine()->getManager();
-						$entityManager->persist($produit);
-						$entityManager->flush();
-
-						// ... do any other work - like sending them an email, etc
-						// maybe set a "flash" success message for the user
-
-						return $this->redirectToRoute('ex_grumpy_add_product');
-				}
-
-				return $this->render(
-						'@EXGrumpy/Forum/add_product.html.twig',
-						array('form' => $form->createView())
-				);
-		}
-
-		public function add_commandeAction(Request $request)
-		{
-
-			$commande = new Commande();
-			$form = $this->createForm(CommandeType::class, $commande);
-
-
-			$form->handleRequest($request);
-
-			if ($form->isSubmitted() && $form->isValid()) {
-
-						$entityManager = $this->getDoctrine()->getManager();
-						$entityManager->persist($commande);
-						$entityManager->flush();
-
-						// ... do any other work - like sending them an email, etc
-						// maybe set a "flash" success message for the user
-
-						return $this->redirectToRoute('ex_grumpy_add_commande');
-				}
-
-				return $this->render(
-						'@EXGrumpy/Forum/add_commande.html.twig',
-						array('form' => $form->createView())
-				);
-		}
 
 		public function add_commentaireAction(Request $request, $cat_comment, $event_id)
 		{
@@ -349,52 +299,5 @@ class ForumController extends Controller
 
 		}
 
-		public function view_productsAction(Request $request) {
-			$user = $this->getUser();
-			if ($user == null) {
-				return $this->redirectToRoute('fos_user_security_login');
-			}
-
-			$products = $this->getDoctrine()
-				->getRepository(Produit::class)
-				->findAll();
-
-			$temp = [];
-			foreach ($products as &$product) {
-				$temp[] = 
-				[
-					"name" => $product->getNom(),
-					"price" => $product->getPrix() . '€',
-					"description" => $product->getDescription(),
-					"category" => $product->getCategorie(),
-					"chemin_image" => "http://via.placeholder.com/350x150"
-				];
-			}
-
-			unset($products);
-
-			return $this->render('@EXGrumpy/Forum/view_products.html.twig', ['products' => $temp]);
-		}
-
-		public function view_productAction($product_id) {
-			$user = $this->getUser();
-			if ($user == null) {
-				return $this->redirectToRoute('fos_user_security_login');
-			}
-
-			$product = $this->getDoctrine()
-				->getRepository(Produit::class)
-				->find($product_id);
-
-			$product = 
-			[
-				"name" => $product->getNom(),
-				"price" => $product->getPrix() . '€',
-				"description" => $product->getDescription(),
-				"category" => $product->getCategorie(),
-				"chemin_image" => "http://via.placeholder.com/350x150"
-			];
-
-			return $this->render('@EXGrumpy/Forum/view_product.html.twig', $product);
-		}
+		
 }
